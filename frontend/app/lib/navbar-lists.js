f13d3aa84e52a648e494eb3c4dbb26f7ca8c2434
auna007 <awwaliliyasu@gmail.com>
2024-05-20 23:35:50 +0100

    Created the header and hero section

---
+++ frontend/app/lib/navbar-lists.js
@@ -0,0 +1,52 @@
+import FeatureIcon from '../../public/icons/features.svg';
+import SchedulesIcon from '../../public/icons/schedules.svg';
+import ResultIcon from '../../public/icons/result.svg';
+import RankingIcon from '../../public/icons/ranking.svg';
+import VideosIcon from '../../public/icons/videos.svg';
+import ForumIcon from '../../public/icons/forum.svg';
+import SearchIcon from '../../public/icons/search.svg';
+import LoginSignupIcon from '../../public/icons/login-signup.svg';
+
+
+export const navlists = [
+    {
+        name: 'FEATURES',
+        icon: FeatureIcon,
+        link: ''
+    },
+    {
+        name: 'SCHEDULES',
+        icon: SchedulesIcon,
+        link: ''
+    },
+    {
+        name: 'RESULT',
+        icon: ResultIcon,
+        link: ''
+    },
+    {
+        name: 'RANKING',
+        icon: RankingIcon,
+        link: ''
+    },
+    {
+        name: 'VIDEOS',
+        icon: VideosIcon,
+        link: ''
+    },
+    {
+        name: 'FORUM',
+        icon: ForumIcon,
+        link: ''
+    },
+    {
+        name: 'SEARCH',
+        icon: SearchIcon,
+        link: ''
+    },
+    {
+        name: 'SIGN IN/LOGIN',
+        icon: LoginSignupIcon,
+        link: ''
+    },
+]
